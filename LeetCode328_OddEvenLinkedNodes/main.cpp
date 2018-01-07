c5504685a6df2956fea56daa0ecde0e9e8b709ca
Rui <rui@dhcp154.merusouth116.iit.edu>
2018-01-06 23:37:26 -0600

    first commit

--- LeetCode328_OddEvenLinkedNodes/main.cpp
+++ LeetCode328_OddEvenLinkedNodes/main.cpp
@@ -13,3 +13,34 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     return 0;
 }
+
+ // Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        
+        while(even && even->next)
+        {
+            ListNode* temp = even->next;
+            
+            even->next = temp->next;
+            temp->next = odd->next;
+            odd->next = temp;
+            
+            odd = odd->next;
+            even = even->next;
+        }
+        
+        return head;
+        
+    }
+};
